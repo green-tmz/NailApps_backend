5583d57cb7183ad4db41090d25f7268444fb607e
green-tmz <ya.ruslan2013@mail.ru>
2025-06-07 13:22:19 +0300

    Рефакторинг

--- app/Modules/Service/Http/Resources/ServiceResource.php
+++ app/Modules/Service/Http/Resources/ServiceResource.php
@@ -30,6 +30,7 @@ class ServiceResource extends JsonResource
             'price' => $this->price,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'updated_at2' => $this->updated_at,
         ];
     }
 }
